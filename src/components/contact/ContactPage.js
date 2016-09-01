085b39c6de24c1c204440708840f19922541dba9
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-09-01 01:59:16 -0700

    Added ContactPage component, set up the routing for it, and added a link to it in the header.

---
+++ src/components/contact/ContactPage.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+class ContactPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Contact Me</h1>
+			</div>
+		);
+	}
+}
+
+export default ContactPage;
