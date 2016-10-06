a09df1ad5c3017f7a55bcb0e43884e3e6567f89e
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-10-06 16:41:35 -0700

    Ability to add a course with notification and error handling with the messages displayed  by toastr.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,43 +3,53 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
 
-	constructor(props, context) {
-		super(props, context);
-	}
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
 
-	courseRow(course, index) {
-		return <div key={index}>{course.title}</div>;
-	}
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
 
-	render() {
-		const {courses} = this.props
-		return (
-			<div>
-				<h1>Courses</h1>
-				<CourseList courses={courses}/>
-			</div>
-		);
-	}
+  render() {
+    const {courses} = this.props;
+
+    return (
+      <div>
+        <h1>Courses</h1>
+        <input type="submit"
+               value="Add Course"
+               className="btn btn-primary"
+               onClick={this.redirectToAddCoursePage}/>
+        <CourseList courses={courses}/>
+      </div>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-	courses: PropTypes.array.isRequired,
-	actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-				return {
-				courses: state.courses
-			};
+  return {
+    courses: state.courses
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(courseActions, dispatch)
-	};
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-		<Route path="courses" component={CoursesPage} />
+    <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursePage} />
+    <Route path="course/:id" component={ManageCoursePage} />
   </Route>
 );
