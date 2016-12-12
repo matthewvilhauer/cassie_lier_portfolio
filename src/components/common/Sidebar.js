d405c799ea9505114456b25104348eca48f4f4d8
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-12-11 20:51:04 -0800

    Adds sticky sidebar.

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -3,7 +3,7 @@ import { Link, IndexLink } from 'react-router';
 
 const Sidebar = () => {
   return (
-  	<div id="navbar-container">
+  	<div className="sidebar-container">
 				<h1>Matthew Vilhauer</h1>
 				<h3><IndexLink to="/" activeClassName="active">Projects</IndexLink></h3>
 				<h3><Link to="/about" activeClassName="active">About</Link></h3>
