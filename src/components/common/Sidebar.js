d184133234cced41cbdc4455496674424b6b9fe4
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-12-11 21:38:04 -0800

    Adds contact page with icons linked to email, Github, LinkedIn, and resume.

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
 				<h1>Matthew Vilhauer</h1>
 				<h3><IndexLink to="/" activeClassName="active">Projects</IndexLink></h3>
 				<h3><Link to="/about" activeClassName="active">About</Link></h3>
-				<h3><Link to="/about" activeClassName="active">Contact</Link></h3>
+				<h3><Link to="/contact" activeClassName="active">Contact</Link></h3>
 		</div>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import ProjectsPage from './components/project/ProjectsPage';
 import AboutPage from './components/about/AboutPage';
+import ContactPage from './components/contact/ContactPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={ProjectsPage} />
     <Route path="about" component={AboutPage}/>
+		<Route path="contact" component={ContactPage}/>
   </Route>
 );
