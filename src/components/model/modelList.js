e77eec5cd9c5c2c1fe49b14215a51e2d01712ab1
Matthew Vilhauer <matthewvilhauer@gmail.com>
2017-01-14 19:08:13 -0800

    Adds react-images module for Lightbox component. Also adds Gallery component borrowed from the react-images repo. Modifies the initial state to pass correct props for the Gallery. Also modifies modelList component to use Gallery component instead of Model.

--- src/components/model/modelList.js
+++ src/components/model/modelList.js
@@ -1,12 +1,20 @@
 import React, {PropTypes} from 'react';
-import Model from './model';
+import Gallery from '../common/Gallery';
 
 const ModelList = ({models}) => {
   return (
     <div className="model-list-container">
-				{models.map(model =>
-					<Model key={model.id} model={model}/>
-				)}
+			{models.map(model =>
+				<div className="model-container col-md-4 col-sm-6"
+						 key={model.id}>
+					<Gallery
+						images={model.images}
+						heading={model.title}
+						subheading={model.subtitle}
+						showThumbnails
+					/>
+				</div>
+			)}
 		</div>
   );
 };
