368184214e8d1979563248ffc0758571d02fb667
Matthew Vilhauer <matthewvilhauer@gmail.com>
2017-01-16 20:49:31 -0800

    Adds posters to initialState in the updated format.

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -476,160 +476,130 @@ export default {
 	posters: [
 		{
 			id: 1,
-			title: 'model Fox',
-			pages: ['projects', 'posters'],
+			title: 'Schooley at Arlene\'s - Spring 2016',
+			pages: ['posters'],
 			images: [
 				{
 					id: 1,
-					src: 'images/3d_fox_1_of_4.jpg',
-					thumbnail: 'images/3d_fox_1_of_4.jpg',
+					src: '/images/schooley_arlenes.jpg',
+					thumbnail: '/images/schooley_arlenes.jpg',
 					srcset: [
-						'images/3d_fox_1_of_4.jpg',
+						'/images/schooley_arlenes.jpg',
 					],
-					caption: 'First Picture',
+					caption: 'Schooley at Arlene\'s - Spring 2016',
 					isPreviewImage: true,
-				},
-				{
-					id: 2,
-					src: 'images/3d_fox_2_of_4.jpg',
-					thumbnail: 'images/3d_fox_2_of_4.jpg',
-					srcset: [
-						'images/3d_fox_2_of_4.jpg',
-					],
-					caption: 'Second Picture',
-					isPreviewImage: false,
-				},
-				{
-					id: 3,
-					src: 'images/3d_fox_3_of_4.jpg',
-					thumbnail: 'images/3d_fox_3_of_4.jpg',
-					srcset: [
-						'images/3d_fox_3_of_4.jpg',
-					],
-					caption: 'Third Picture',
-					isPreviewImage: false,
 				}
 			],
-			thumbnail: '/thumbnails/3d_fox_1_of_4.jpg',
-			description: 'model images designed with Maya'
+			description: 'Schooley at Arlene\'s - Spring 2016'
 		},
 		{
 			id: 2,
-			title: 'model Fox2',
-			pages: ['projects', 'illustrations'],
+			title: 'Schooley Mountain Band at Arlene\'s - Summer 2016',
+			pages: ['posters'],
 			images: [
 				{
 					id: 1,
-					src: 'images/3d_fox_1_of_4.jpg',
-					thumbnail: 'images/3d_fox_1_of_4.jpg',
+					src: '/images/schooley_arlene.jpg',
+					thumbnail: '/images/schooley_arlene.jpg',
 					srcset: [
-						'images/3d_fox_1_of_4.jpg',
+						'/images/schooley_arlene.jpg',
 					],
-					caption: 'Test',
+					caption: 'Schooley Mountain Band at Arlene\'s - Summer 2016',
 					isPreviewImage: true,
-				},
-				{
-					id: 2,
-					src: 'images/3d_fox_2_of_4.jpg',
-					thumbnail: 'images/3d_fox_2_of_4.jpg',
-					srcset: [
-						'images/3d_fox_2_of_4.jpg',
-					],
-					caption: 'Test',
-					isPreviewImage: false,
-				},
-				{
-					id: 3,
-					src: 'images/3d_fox_3_of_4.jpg',
-					thumbnail: 'images/3d_fox_3_of_4.jpg',
-					srcset: [
-						'images/3d_fox_3_of_4.jpg',
-					],
-					caption: 'Test',
-					isPreviewImage: false,
 				}
 			],
-			thumbnail: '/thumbnails/3d_fox_1_of_4.jpg',
-			description: 'model images designed with Maya'
+			description: 'Schooley Mountain Band at Arlene\'s - Summer 2016'
 		},
 		{
 			id: 3,
-			title: 'model Fox',
-			pages: ['projects', 'posters'],
+			title: 'Southern Belles - Fall Tour 2016',
+			pages: ['posters'],
 			images: [
 				{
 					id: 1,
-					src: 'images/3d_fox_1_of_4.jpg',
-					thumbnail: 'images/3d_fox_1_of_4.jpg',
+					src: '/images/southern_belles_fall_2016.jpg',
+					thumbnail: '/images/southern_belles_fall_2016.jpg',
 					srcset: [
-						'images/3d_fox_1_of_4.jpg',
+						'/images/southern_belles_fall_2016.jpg',
 					],
-					caption: 'First Picture',
+					caption: 'Southern Belles - Fall Tour 2016',
 					isPreviewImage: true,
-				},
-				{
-					id: 2,
-					src: 'images/3d_fox_2_of_4.jpg',
-					thumbnail: 'images/3d_fox_2_of_4.jpg',
-					srcset: [
-						'images/3d_fox_2_of_4.jpg',
-					],
-					caption: 'Second Picture',
-					isPreviewImage: false,
-				},
-				{
-					id: 3,
-					src: 'images/3d_fox_3_of_4.jpg',
-					thumbnail: 'images/3d_fox_3_of_4.jpg',
-					srcset: [
-						'images/3d_fox_3_of_4.jpg',
-					],
-					caption: 'Third Picture',
-					isPreviewImage: false,
 				}
 			],
-			thumbnail: '/thumbnails/3d_fox_1_of_4.jpg',
-			description: 'model images designed with Maya'
+			description: 'Southern Belles - Fall Tour 2016'
 		},
 		{
 			id: 4,
-			title: 'model Fox2',
-			pages: ['projects', 'illustrations'],
+			title: 'Damn Tall Buildings & Red Light Rodeo - Fall 2016',
+			pages: ['posters'],
 			images: [
 				{
 					id: 1,
-					src: 'images/3d_fox_1_of_4.jpg',
-					thumbnail: 'images/3d_fox_1_of_4.jpg',
+					src: '/images/jet_trails_media.jpg',
+					thumbnail: '/images/jet_trails_media.jpg',
 					srcset: [
-						'images/3d_fox_1_of_4.jpg',
+						'/images/jet_trails_media.jpg',
 					],
-					caption: 'Test',
+					caption: 'Damn Tall Buildings & Red Light Rodeo - Fall 2016',
 					isPreviewImage: true,
-				},
-				{
-					id: 2,
-					src: 'images/3d_fox_2_of_4.jpg',
-					thumbnail: 'images/3d_fox_2_of_4.jpg',
-					srcset: [
-						'images/3d_fox_2_of_4.jpg',
-					],
-					caption: 'Test',
-					isPreviewImage: false,
-				},
-				{
-					id: 3,
-					src: 'images/3d_fox_3_of_4.jpg',
-					thumbnail: 'images/3d_fox_3_of_4.jpg',
-					srcset: [
-						'images/3d_fox_3_of_4.jpg',
-					],
-					caption: 'Test',
-					isPreviewImage: false,
 				}
 			],
-			thumbnail: '/thumbnails/3d_fox_1_of_4.jpg',
-			description: 'model images designed with Maya'
-		}
+			description: 'Damn Tall Buildings & Red Light Rodeo - Fall 2016'
+		},
+		{
+			id: 5,
+			title: 'Litz, Moogatu, & Microwaves - Fall 2016',
+			pages: ['posters'],
+			images: [
+				{
+					id: 1,
+					src: '/images/litz_moogatu.jpg',
+					thumbnail: '/images/litz_moogatu.jpg',
+					srcset: [
+						'/images/litz_moogatu.jpg',
+					],
+					caption: 'Litz, Moogatu, & Microwaves - Fall 2016',
+					isPreviewImage: true,
+				}
+			],
+			description: 'Litz, Moogatu, & Microwaves - Fall 2016'
+		},
+		{
+			id: 6,
+			title: 'Denizen Release Show - Winter 2017',
+			pages: ['posters'],
+			images: [
+				{
+					id: 1,
+					src: '/images/schooley_denizen.jpg',
+					thumbnail: '/images/schooley_denizen.jpg',
+					srcset: [
+						'/images/schooley_denizen.jpg',
+					],
+					caption: 'Denizen Release Show - Winter 2017',
+					isPreviewImage: true,
+				}
+			],
+			description: 'Denizen Release Show - Winter 2017'
+		},
+		{
+			id: 7,
+			title: 'Schooley Mountain Band at Arlene\'s - January 2017',
+			pages: ['posters'],
+			images: [
+				{
+					id: 1,
+					src: '/images/schooley_jouwala.jpg',
+					thumbnail: '/images/schooley_jouwala.jpg',
+					srcset: [
+						'/images/schooley_jouwala.jpg',
+					],
+					caption: 'Schooley Mountain Band at Arlene\'s - January 2017',
+					isPreviewImage: true,
+				}
+			],
+			description: 'Schooley Mountain Band at Arlene\'s - January 2017'
+		},
 	],
 	lightboxIsOpen: true
 };
